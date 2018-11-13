4fa4b0356abd94138dce444aa37e0734f9ca5dc9
Frank Fischer <fischer.frank@gmail.com>
2018-11-13 16:30:45 +0100

    -

---
+++ step_1_download_raw_data.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo '=== nyc taxi to airport - step 1 download raw data'
+
+URL='https://data.cityofnewyork.us/api/views/biws-g3hs/rows.csv?accessType=DOWNLOAD'
+FILE='nyc-2017-yellow-taxi-trips.cvs.gz'
+
+if [ -e $FILE ]
+then
+    echo "output file exists: $FILE"
+    echo "skipping"
+    exit 0
+fi
+
+curl "$URL" | gzip --best > $FILE

59fad3b4a03776ffa5f6273b9ec0d00ff679bc3d
Frank Fischer <fischer.frank@gmail.com>
2018-11-13 18:21:38 +0100

    -

--- step_1_download_raw_data.sh
+++ step_1_download_raw_data.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 echo '=== nyc taxi to airport - step 1 download raw data'
+echo
 
 URL='https://data.cityofnewyork.us/api/views/biws-g3hs/rows.csv?accessType=DOWNLOAD'
 FILE='nyc-2017-yellow-taxi-trips.cvs.gz'
-
+                                
 if [ -e $FILE ]
 then
     echo "output file exists: $FILE"
@@ -12,4 +13,11 @@ then
     exit 0
 fi
 
+echo "downloading from url: $URL"
+echo "compressing and writing to file: $FILE"
+echo
+
 curl "$URL" | gzip --best > $FILE
+
+echo
+echo "done"
